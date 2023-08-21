15e35229f0cec07e8ead5359eefad0ba42d05660
dariazaretska <daria.zaretskaya@gmail.com>
2023-08-21 23:01:52 +0200

    homework 19 done - junit calc tests

--- homework_19/src/test/java/com/hillel/homework19/CalculatorTest.java
+++ homework_19/src/test/java/com/hillel/homework19/CalculatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
